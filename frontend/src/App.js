a02b4843a8c6b50bdc0f9e7498a0ec58902fb7eb
unknown <fpsantos86@hotmail.com>
2020-01-17 01:07:38 -0300

    Adding FrontEnd folder

---
+++ frontend/src/App.js
@@ -0,0 +1,153 @@
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
+
+// Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação
+// Propriedade: Informações que um componente PAI passa para o componente FILHO.
+// Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
+
+function App() {
+  const [github_username, setGithubUsername] = useState('');
+  const [techs,setTechs] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setLatitude(latitude);
+        setLongitude(longitude);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000,
+      }
+    )
+  }, []);
+
+  async function handleAddDev(e){
+    e.preventDefault();
+    const response = await api.post('/devs',
+    {
+      github_username,
+      techs,
+      latitude,
+      longitude,
+    })
+
+    setGithubUsername('');
+    setTechs('');
+  }
+
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input 
+              name="github_username" 
+              id="github_username" 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)} />
+
+          </div>
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input 
+              name="techs" 
+              id="techs"
+              required
+              value={techs}
+              onChange={e=> setTechs(e.target.value)} />
+
+          </div>
+
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="number"
+                name="latitude"
+                id="latitude"
+                required
+                value={latitude}
+                onChange={e=> setLatitude(e.target.value)} />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="number"
+                name="longitude"
+                id="longitude"
+                required
+                value={longitude}
+                onChange={e=> setLongitude(e.target.value)}  />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/1225727?s=460&v=4" alt="Felipe Pereira" />
+              <div className="user-info">
+                <strong>Felipe Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Analista Desenvolvedor. Entusiasta dos melhores princípios, padrões e práticas agile.</p>
+            <a href="https://github.com/fpsantos86">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/1225727?s=460&v=4" alt="Felipe Pereira" />
+              <div className="user-info">
+                <strong>Felipe Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Analista Desenvolvedor. Entusiasta dos melhores princípios, padrões e práticas agile.</p>
+            <a href="https://github.com/fpsantos86">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/1225727?s=460&v=4" alt="Felipe Pereira" />
+              <div className="user-info">
+                <strong>Felipe Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Analista Desenvolvedor. Entusiasta dos melhores princípios, padrões e práticas agile.</p>
+            <a href="https://github.com/fpsantos86">Acessar perfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/1225727?s=460&v=4" alt="Felipe Pereira" />
+              <div className="user-info">
+                <strong>Felipe Pereira</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Analista Desenvolvedor. Entusiasta dos melhores princípios, padrões e práticas agile.</p>
+            <a href="https://github.com/fpsantos86">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
+}
+
+export default App;
